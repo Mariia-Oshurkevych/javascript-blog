8a263ff5487bf6af2d202cb5988a65967d67de45
Mariia Oshurkevych <oshurkevich2@gmail.com>
2021-02-10 00:41:24 +0100

    Add class:active to the clicked element

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 'use strict';
 function titleClickHandler(event){
+	const clickedElement = this;
   console.log('Link was clicked!');
-  console.log(event);
   /* remove class 'active' from all article links  */
 const activeLinks = document.querySelectorAll('.titles a.active');
 
@@ -9,6 +9,8 @@ for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
   /* add class 'active' to the clicked link */
+  console.log('clickedElement:', clickedElement);
+  clickedElement.classList.add('active');
 
   /* remove class 'active' from all articles */
 const activeArticles = document.querySelectorAll('.posts article.active');
