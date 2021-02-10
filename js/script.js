f69fa06ce783d658a785323bcf73c352dc8e5687
Mariia Oshurkevych <oshurkevich2@gmail.com>
2021-02-10 01:11:36 +0100

    Finish titleClickHandler function

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 'use strict';
 function titleClickHandler(event){
+	event.preventDefault();
 	const clickedElement = this;
   console.log('Link was clicked!');
   /* remove class 'active' from all article links  */
@@ -19,10 +20,13 @@ for(let activeArticle of activeArticles){
   activeArticle.classList.remove('active');
 }
   /* get 'href' attribute from the clicked link */
-
+const articleSelector = clickedElement.getAttribute('href');
+console.log(articleSelector);
   /* find the correct article using the selector (value of 'href' attribute) */
-
+const targetArticle = document.querySelector(articleSelector);
+console.log(targetArticle);
   /* add class 'active' to the correct article */
+targetArticle.classList.add('active');
 }
 
 const links = document.querySelectorAll('.titles a');
